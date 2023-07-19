75a7418180c9ab8425440d9bd16bdad169ad2c28
Nahuel Llancapichun <nahuelmath@gmail.com>
2023-07-19 17:42:49 -0400

    feat: add animation linked list

--- tutorials/data_structures_animations/linked_list/linked_list.py
+++ tutorials/data_structures_animations/linked_list/linked_list.py
@@ -3,10 +3,24 @@ from manim import *
 
 class AnimatedLinkedList(Scene):
     def construct(self):
-        circles = [Create(Circle(color=WHITE).scale(0.5).shift(i * RIGHT).shift(UP))
-                   for i in range(-8, 9, 3)]
 
-        arrows = [Create(Arrow(start=c.get_right(), end=RIGHT))
-                  for c in circles[0:len(circles)-1]]
+        circles: list[Mobject] = list()
+        arrows: list[Mobject] = list()
+        node_values: list[Text] = list()
 
-        self.play(*circles, )
+        for i in range(-1, 9, 3):
+            text = Text(str(i), font_size=48, color=BLUE_D)
+            circle = Circle(color=WHITE).shift(i * RIGHT)
+            text = text.shift(circle.get_center())
+            circle = circle.surround(text)
+            circles.append(circle)
+            node_values.append(text)
+            arrows.append(Arrow(start=circle.get_right(),
+                          end=circle.get_right() + RIGHT*2))
+
+        circles_animations = list(map(Create, circles))
+        node_values_animations = list(map(Create, node_values))
+        arrows_animations = list(map(Create, arrows))
+
+        self.play(*circles_animations, *
+                  node_values_animations, *arrows_animations)
