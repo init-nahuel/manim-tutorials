fe3ca3d24ac82d2eaf137fe9c609ceea46272746
Nahuel Llancapichun <nahuelmath@gmail.com>
2023-07-20 19:21:30 -0400

    fix: using newfunction for creating ll (still need refactor)

--- tutorials/data_structures_animations/linked_list/linked_list.py
+++ tutorials/data_structures_animations/linked_list/linked_list.py
@@ -2,7 +2,7 @@ from manim import *
 import numpy as np
 
 
-def create_ll_animation(coords: list[np.ndarray], nodes_scale=1, text_scale=1, arrows_scale=1) -> tuple[list[Mobject]]:
+def create_ll_animation(coords: list[np.ndarray], nodes_scale=1, text_scale=1, arrows_scale=1) -> tuple[list[Circle], list[Text], list[Arrow]]:
     """Create the linked list mobjects and positioning on the given coordinates.
     Returns a tuple with each list containing the mobjects for nodes,
     node's value and arrows."""
@@ -12,9 +12,11 @@ def create_ll_animation(coords: list[np.ndarray], nodes_scale=1, text_scale=1, a
     arrows: list[Mobject] = list()
 
     if (len(coords) == 1):
-        nodes.append(Circle(color=WHITE).shift(coords[0]).scale(nodes_scale))
-        node_values.append(Text(str(abs(int(i[0])))).scale(text_scale))
-        return (nodes, node_values, arrow)
+        pos = coords[0]
+        nodes.append(Circle(color=WHITE).shift(pos).scale(nodes_scale))
+        node_values.append(Text(str(abs(int(pos[0]))), color=BLUE_D, font_size=48).shift(
+            pos).scale(text_scale))
+        return (nodes, node_values, None)
 
     arrows_len = abs(coords[1] - coords[0]) - 2*np.array((nodes_scale, 0, 0))
 
@@ -36,45 +38,36 @@ def create_ll_animation(coords: list[np.ndarray], nodes_scale=1, text_scale=1, a
     return (nodes, node_values, arrows)
 
 
-class AnimatedAddNodeToLinkedList(Scene):
+class AnimatedAddNode(Scene):
     def construct(self):
-
-        nodes: list[Mobject] = list()
-        arrows: list[Mobject] = list()
-        node_values: list[Text] = list()
-
-        for i in range(-1, 9, 3):
-            text = Text(str(abs(i)), font_size=48, color=BLUE_D)
-            circle = Circle(color=WHITE).shift(i * RIGHT)
-            text = text.shift(circle.get_center())
-            circle = circle.surround(text).scale(1.5)
-            nodes.append(circle)
-            node_values.append(text)
-            arrows.append(Arrow(start=circle.get_right(),
-                          end=circle.get_right() + RIGHT*2))
-        arrows.pop()
-
-        nodes_animations = list(map(Create, nodes))
-        node_values_animations = list(map(Create, node_values))
-        arrows_animations = list(map(Create, arrows))
+        coords = [(0, 0, 0), (3, 0, 0), (6, 0, 0), (9, 0, 0)]
+        coords = list(map(np.array, coords))
+        nodes, node_values, arrows = create_ll_animation(
+            coords, nodes_scale=0.8)
 
         # Show linked list before insertion
+        nodes_animations = list(map(Create, nodes))
+        node_value_animations = list(map(Create, node_values))
+        arrows_animations = list(map(Create, arrows))
         self.play(*nodes_animations,
-                  *node_values_animations,
+                  *node_value_animations,
                   *arrows_animations)
 
-        new_node_pos = nodes[0].get_center() - RIGHT*3
-        new_node_value = Text(str(9), font_size=48,
-                              color=BLUE_D).shift(new_node_pos)
-        new_node = Circle(color=WHITE).shift(new_node_pos).surround(
-            new_node_value).scale(1.5)
+        new_node_coords = list([np.array((-3, 0, 0))])
+        new_node, new_node_val, _ = create_ll_animation(
+            new_node_coords, nodes_scale=0.8)
+        new_node = new_node[0]
+        new_node_val = new_node_val[0]
+
+        arrows_len = abs(coords[1] - coords[0]) - 2 * \
+            np.array((0.8, 0, 0))
+        new_arrow = Arrow(start=new_node.get_right(),
+                          end=new_node.get_right()+arrows_len)
 
         # Show new node added
         self.wait(1)
-        self.play(Create(Text("addNode(9)", font_size=48,
-                  color=BLUE_D).shift(new_node_pos + DOWN)))
+        self.play(Create(Text("addNode(3)", font_size=48,
+                              color=BLUE_D).shift(new_node_coords + DOWN*2)))
         self.wait(1)
-        self.play(Create(new_node), Create(
-            Arrow(start=new_node.get_right(), end=new_node.get_right() + RIGHT*2)),
-            Create(new_node_value))
+        self.play(Create(new_node), Create(new_node_val), Create(new_arrow))
         self.wait(2)
