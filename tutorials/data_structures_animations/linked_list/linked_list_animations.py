8f726f1a9554810a21def2b0f383eb8b387ba9b1
Nahuel <nahuelmath@gmail.com>
2023-07-22 17:12:12 -0400

    feat: add complete insertNode function

--- tutorials/data_structures_animations/linked_list/linked_list_animations.py
+++ tutorials/data_structures_animations/linked_list/linked_list_animations.py
@@ -2,7 +2,7 @@ from manim import *
 import numpy as np
 
 
-def create_ll(coords: list[np.array], node_scale=1, text_scale=1, arrow_scale=1) -> VGroup:
+def create_ll(coords: list[np.array], node_scale=1, text_scale=1, arrow_scale=1, node_value=None) -> VGroup:
     """Create a linked list composed of mobjects (`Circle`, `Text` and `Arrow`) for the animation, it links
     the nodes sequentially. Returns a `VGroup` containing the mobjects.
     """
@@ -13,6 +13,8 @@ def create_ll(coords: list[np.array], node_scale=1, text_scale=1, arrow_scale=1)
         pos = coords[0]
         circle = Circle(color=WHITE).shift(pos).scale(node_scale)
         value = Text(str(abs(int(pos[0]))), color=BLUE_D).shift(pos).scale(text_scale)
+        if node_value:
+            value = Text(str(node_value[0]), color=BLUE_D).shift(pos).scale(text_scale)
         vgroup.add(circle, value)
         return vgroup
     
@@ -20,13 +22,22 @@ def create_ll(coords: list[np.array], node_scale=1, text_scale=1, arrow_scale=1)
     prev_coord = coords[0]
     circle = Circle(color=WHITE).shift(prev_coord).scale(node_scale)
     value = Text(str(abs(int(prev_coord[0]))), color=BLUE_D).shift(prev_coord).scale(text_scale)
+
+    if node_value:
+        value = Text(str(node_value[0]), color=BLUE_D).shift(c).scale(text_scale)
+
     vgroup.add(circle, value)
 
-    for c in coords[1:]:
+    for i in range(1, len(coords)):
+        c = coords[i]
         arrow_len = abs(c - prev_coord) - 2*np.array((node_scale, 0, 0))
         arrow = Arrow(start=circle.get_right(), end=circle.get_right()+arrow_len).scale(arrow_scale)
         circle = Circle(color=WHITE).shift(c).scale(node_scale)
         value = Text(str(abs(int(c[0]))), color=BLUE_D).shift(c).scale(text_scale)
+        
+        if node_value:
+            value = Text(str(node_value[i]), color=BLUE_D).shift(c).scale(text_scale)
+        
         prev_coord = c
         vgroup.add(circle, value, arrow)
     
@@ -106,4 +117,62 @@ class AnimatedRemoveNode(Scene):
 
         # Resize arrow to original scale
         self.play(ll_part1.animate.shift(RIGHT*3), prev_link.animate.shift(RIGHT*1.4).scale(0.3))
+        self.wait(2)
+
+class AnimatedInsertNode(Scene):
+    def construct(self):
+        part1_coords = np.array([(-3, 0, 0), (0, 0, 0), (3, 0, 0)])
+        ll_part1 = create_ll(part1_coords, node_scale=0.8)
+
+        part2_coords = np.array([(6, 0, 0)])
+        ll_part2 = create_ll(part2_coords, node_scale=0.8)
+
+        arrow_len = part1_coords[-1] - 2 * np.array((0.8, 0, 0))
+        connect_lists = Arrow(start=part1_coords[-1]+RIGHT*0.8, end=part1_coords[-1]+RIGHT*0.8+arrow_len)
+
+        # Show list before inserting new node
+        self.play(Create(ll_part1, lag_ratio=0),
+                  Create(ll_part2, lag_ratio=0),
+                  Create(connect_lists))
+        self.wait(1)
+
+        # Show text operation: insertNode(7, 2)
+        insert_node_txt = Text("insertNode(value=7, index=2)", font_size=48, color=BLUE_D).shift(DOWN*2)
+        self.play(Create(insert_node_txt))
+        self.wait(1)
+
+        # Show node's indexs
+        part1_indexs = [Text(str(i), font_size=48, color=ORANGE).shift(part1_coords[i] + 1.5*UP).scale(0.8)
+                        for i in range(len(part1_coords))]
+        part1_indexs = VGroup(*part1_indexs)
+        
+        self.play(Create(part1_indexs))
+        part2_index = Text(str(3), font_size=48, color=ORANGE).shift(part2_coords[0] + 1.5*UP).scale(0.8)
+        self.play(Create(part2_index))
+        self.wait(1)
+
+        # Show pointer searching the target index
+        ptr = Arrow(start=part1_coords[0] + UP*3, end=part1_coords[0] + 1.5*UP, color=ORANGE)
+        self.play(Create(ptr))
+        self.wait(0.5)
+        for i in range(2):
+            self.play(ptr.animate.shift(RIGHT*3))
+            self.wait(0.5)
+        self.wait(1)
+        self.play(FadeOut(ptr))
+
+        # Show insertion new node
+        self.play(ll_part2.animate.shift(RIGHT*3))
+
+        new_node_pos = np.array((6, 0, 0))
+        new_node = create_ll([new_node_pos], node_scale=0.8, node_value=[7])
+        self.play(Create(new_node))
+        self.wait(1)
+
+        # Show link between new node and index
+        new_index = Text(str(4), font_size=48, color=ORANGE).shift(part2_coords[0] + 3*RIGHT + 1.5*UP).scale(0.8)
+        new_link = Arrow(start=part1_coords[-1]+RIGHT*0.8, end=part1_coords[-1]+RIGHT*0.8+arrow_len).shift(RIGHT*3)
+
+        self.play(Create(new_index),
+                  Create(new_link))
         self.wait(2)
